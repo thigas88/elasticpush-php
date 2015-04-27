19d9f08b8cbda75c30aab268d89f768e7504dc0f
André Kiffer <andre_kiffer@hotmail.com>
2015-04-27 07:56:56 -0300

    add more keyworkds in composer.json

--- examples/notify.php
+++ examples/notify.php
@@ -3,7 +3,7 @@
 require '../vendor/autoload.php';
 
 $access_token = '182cebb17d71d3b2b2cd79b03dc2e74a82d687e4';
-// $secret = '182cebb17d71d3b2b2cd79b03dc2e74a82d687e4';
+//$secret = '182cebb17d71d3b2b2cd79b03dc2e74a82d687e4';
 // $access_token = $token . ':' . $secret;
 
 $elasticpush = new Elasticpush($access_token, 1100);
