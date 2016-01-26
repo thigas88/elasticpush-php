982e54d1c573b53fd46825cc5af64db31a4db0c9
André Kiffer <andre_kiffer@hotmail.com>
2016-01-26 00:24:05 -0200

    Adding support to guzzle ~6

--- examples/notify.php
+++ examples/notify.php
@@ -2,15 +2,20 @@
 
 require '../vendor/autoload.php';
 
+$token = '2e4c651f7199dd10c8ed6ef56ef3337e1898b521994fafb9e2b5ec9a99a04bac';
+$secretToken = '169843c1d4b791ed5bb2ed2e1f90991a3f6b8dc2b40f094d84e1f4cbc70dfd5d';
+$appId = 2;
+$elasticpush = (new Elasticpush(
+                    sprintf('%s:%s', $token, $secretToken),
+                    $appId
+                ))->setClientId(0);
 
-$key = '2e4c651f7199dd10c8ed6ef56ef3337e1898b521994fafb9e2b5ec9a99a04bac';
-$secret = '169843c1d4b791ed5bb2ed2e1f90991a3f6b8dc2b40f094d84e1f4cbc70dfd5d';
-$elasticpush = new Elasticpush( $key . ':' . $secret, 2);
+$event = $elasticpush->dispatch(
+    'elasticpush-test',
+    'event-test', [
+        'message' => 'Teste',
+        'title' => 'Testando'
+    ]
+);
 
-$elasticpush->setClientId(0);
-
-$event = $elasticpush->dispatch('elasticpush-test', 'event-test', [
-      	'message' => 'Teste',
-	'title' => 'Testando'
-]);
-
+var_dump($event);
--- lib/Elasticpush.php
+++ lib/Elasticpush.php
@@ -1,51 +1,95 @@
 <?php
 
 use \GuzzleHttp\Client;
+use \GuzzleHttp\Exception\ClientException;
 
+/**
+ * Class Elasticpush
+ */
 class Elasticpush
 {
-	const API_VERSION = 'v1';
+    const API_VERSION = 'v1';
 
-	private $settings = [
-				'host' => 'https://api.elasticpush.com/',
-				'api_version' => self::API_VERSION
-			];
+    const HOST = 'https://api.elasticpush.com/';
 
-	private $client;
-	public function __construct($token, $app_id)
-	{
-		$endpoint = $this->settings['host'] . $this->settings['api_version'] . '/apps/' . $app_id . '/';
-		$token = (strpos($token, ':')!==false) ? explode(':', $token) : trigger_error('Invalid token format');
-		$this->client = new Client([
-				'base_url' => $endpoint,
-				'defaults' => [
-					'headers' => [
-						'Content-Type' => 'application/json',
-						'Accept' => 'application/json',
-						'X-Token' => $token[0],
-						'X-Secret-Token' => $token[1],
-					],					
-					'connect_timeout' => 15,
-					'timeout' => 10,
-					'verify' => false
-				]
-			]);
-	}
-	public function setClientId($client_id)
-	{
-		$this->client_id = $client_id;
-		return $this;
-	}
-	public function dispatch($channel, $event, array $data)
-	{
-		$request = [
-			'channel' => $channel,
-			'event' => $event,
-			'data' => $data
-		];
-		if(isset($this->client_id)) 
-			$request['identifier'] = $this->client_id;
+    /** @var Client */
+    private $client;
 
-		return $this->client->post('events', ['json' => $request]);
-	}
+    /**
+     * @param $token
+     * @param $app_id
+     */
+    public function __construct($token, $app_id)
+    {
+        $endpoint = self::HOST . self::API_VERSION . '/apps/' . $app_id . '/';
+        $token = (strpos($token, ':')!==false) ? explode(':', $token) : trigger_error('Invalid token format');
+        $this->client = new Client([
+            'base_uri' => $endpoint,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'X-Token' => reset($token),
+                'X-Secret-Token' => end($token),
+            ],
+            'connect_timeout' => 15,
+            'timeout' => 10,
+            'verify' => false
+        ]);
+    }
+
+    /**
+     * @param $client_id
+     * @return $this
+     */
+    public function setClientId($client_id)
+    {
+        $this->client_id = $client_id;
+        return $this;
+    }
+
+    /**
+     * @param $channel
+     * @param $event
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function dispatch($channel, $event, array $data)
+    {
+        $request = [
+            'channel' => $channel,
+            'event' => $event,
+            'data' => $data
+        ];
+
+        if (isset($this->client_id)) {
+            $request['identifier'] = $this->client_id;
+        }
+
+        try {
+            $response = $this->client->post('events', [
+                'json' => $request
+            ]);
+            if (method_exists($response, 'getBody')
+               && method_exists($response->getBody(), 'gxetContents')) {
+                return json_decode($response->getBody()->getContents(), true);
+            }
+            return [
+                'status' => true
+            ];
+        } catch (ClientException $ce) {
+            $message = $this->prepareException($ce);
+            throw new \RuntimeException($message);
+        }
+    }
+
+    private function prepareException(ClientException $ce) {
+        $message = 'An error occured';
+        $body = json_decode($ce->getResponse()->getBody());
+
+        if (!$body) {
+            return $message;
+        }
+
+        return $body->message;
+    }
 }
